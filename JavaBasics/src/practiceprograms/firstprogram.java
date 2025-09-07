fcb97d4cec5154570bd3477d8600b14e62ec036c
mohit <mohit@admin>
2025-09-07 11:35:48 +0530

    first program commit1

---
+++ JavaBasics/src/practiceprograms/firstprogram.java
@@ -0,0 +1,11 @@
+package practiceprograms;
+
+public class firstprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("mohit parashar");
+
+	}
+
+}
